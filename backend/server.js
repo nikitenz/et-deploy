02733825a24b0433e3e6339a4b51fefebbe8f9a9
nikitenz <nikitam.tenorio@gmail.com>
2025-09-05 10:26:20 +0800

    practice deployment for hosting

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,33 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 4000;
+const __dirname = path.resolve();
 connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
     origin: "http://localhost:5173",
     credentials: true}));
+}
 
 // API Endpoints
 app.get('/', (req, res) => res.send("API Working"));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html" ))
+    });
+}
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+// in production there's no localhost so this has to be dynamic
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "api";
+
+const api = axios.create({
+    baseURL: BASE_URL,
+});
+
+export default api;
